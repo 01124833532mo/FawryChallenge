9fd6b4bf8a58d1bdf7e3fdfc0ca416896b1cfc46
mohamed_hamdi_mahmoud <mohammedhamdi726@gmail.com>
2025-07-05 00:11:39 +0300

    Test Case 1  Normal checkout with shippable items

--- FawryChallenge/Program.cs
+++ FawryChallenge/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace FawryChallenge
+﻿using FawryChallenge.Entities;
+using FawryChallenge.Entities.Products;
+using FawryChallenge.Service.Implementation;
+
+namespace FawryChallenge
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create products
+            var cheese = new ShippableExpirableProduct("Cheese", 100, 10, 0.4, DateTime.Now.AddDays(5));
+            var biscuits = new ShippableExpirableProduct("Biscuits", 150, 5, 0.7, DateTime.Now.AddDays(10));
+            var tv = new ShippableProduct("TV", 2000, 3, 15.5);
+            var mobile = new NonExpirableProduct("Mobile", 800, 8);
+            var scratchCard = new NonExpirableProduct("Mobile Scratch Card", 10, 100);
+
+            // Create customer
+            var customer = new Customer("Mohamed Hamdy Mahmoud", 5000);
+
+            // Create services
+            var shippingService = new ShippingService();
+            var checkoutService = new CheckoutService(shippingService);
+
+            // Test case 1: Normal checkout with shippable items
+
+            var cart1 = new Cart();
+            cart1.Add(cheese, 2);
+            cart1.Add(biscuits, 1);
+            cart1.Add(scratchCard, 1);
+            checkoutService.Checkout(customer, cart1);
+
+
+
         }
     }
 }
